509b6321eaa65085d93bde4261af69b9cf891c9e
adon3s <141533318+sauloadn@users.noreply.github.com>
2024-02-28 10:27:09 -0300

    Create firstclass.py

---
+++ firstclass.py
@@ -0,0 +1,21 @@
+#1 Ida - Policial / Ladrao
+
+#2 Policial volta Sozinho
+#3 Policial vai com Filho
+#4 Policial volta com Ladrao
+#5 Pai vai com Filho
+#6 Pai volta sozinho
+#7 Pai vai com a mae
+#8 Mae Volta Sozinho
+#9 Polical vai com ladrao
+#10 Pai volta sozinho
+#11 Pai vai com a mae
+#12 Mae volta sozinha
+#13 Mae volta com a filha
+#14 Policial volta com o ladrao
+#15 Policial vai com a filha
+#16 Policial volta sozinho
+#17 Policial vai com o ladrao
+
+
+### 2 - 2 // 2 - 2 // 1 - 1
